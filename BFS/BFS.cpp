a2915417ede854ee121f4b3a7185f733d3a787b0
EunSeok Jeon <jes901110@gmail.com>
2018-09-29 07:00:15 +0900

    no message

--- BFS/BFS.cpp
+++ BFS/BFS.cpp
@@ -7,23 +7,23 @@
 
 using namespace std;
 
-int number = 7;
-int c[7];
-vector<int> a[8];
+int number = 15;
+int c[15];
+vector<int> a[16];
 
 void bfs(int start){
 	queue<int> q;
-	q.push(start);
-	c[start] = true;
+	q.push(start); // 1. 시작 노드를 큐에 삽입 
+	c[start] = true; // 2. 시작노드를 방문했다고 처리 해줌 
 	while(!q.empty()){
-		int x = q.front();
-		q.pop();
+		int x = q.front(); 
+		q.pop(); // 3. 큐에서 하나의 노드를 꺼내고 
 		printf("%d ", x);
-		for(int i = 0; i<a[x].size()); i++){
-			int y = a[x][i];
-			if(!c[y]){
-				q.push(y);
-				c[y] = true;
+		for(int i = 0; i<a[x].size(); i++){ // 4. 해당 노드에 연결된 노드들 순회 
+			int y = a[x][i]; // 연결된 노드 
+			if(!c[y]){ // 5. 방문하지 않은 노드이면 
+				q.push(y); // 6.큐에 삽입한다. 
+				c[y] = true; // 7. 방문 처리한다.       3~7을 큐가 빌 때까지 반복한다. 
 			}
 		}		
 	}
@@ -31,6 +31,8 @@ void bfs(int start){
 }
 
 int main(void){
+	
+	/*
 	// 1과 2을 연결합니다. 
 	a[1].push_back(2);
 	a[2].push_back(1);
@@ -38,8 +40,11 @@ int main(void){
 	a[1].push_back(3);
 	a[3].push_back(1);
 	// 2과 3를 연결합니다.
+	
+	//
 	a[2].push_back(3);
 	a[3].push_back(2);
+	//
 	// 2과 4을 연결합니다. 
 	a[2].push_back(4);
 	a[4].push_back(2);
@@ -52,13 +57,33 @@ int main(void){
 	// 3와 7을 연결합니다.
 	a[3].push_back(7);
 	a[7].push_back(3);
-	// 4와 5를 연결합니다.
-	a[4].push_back(5);
-	a[5].push_back(4);
-	// 6과 7을 연결합니다.
-	a[6].push_back(7);
-	a[7].push_back(6);
+	
+	a[4].push_back(8);
+	a[8].push_back(4);
+	
+	a[4].push_back(9);
+	a[9].push_back(4);
+	
+	a[5].push_back(10);
+	a[10].push_back(5);
+	
+	a[5].push_back(11);
+	a[11].push_back(5);
+	
+	a[6].push_back(12);
+	a[12].push_back(6);
+	
+	a[6].push_back(13);
+	a[13].push_back(6);
+	
+	a[7].push_back(14);
+	a[14].push_back(7);
+	
+	a[7].push_back(15);
+	a[15].push_back(7);
+	*/
 	// BFS를 수행합니다.
+	
 	bfs(1);
 
 	return 0;
--- prac/robotVacuum.cpp
+++ prac/robotVacuum.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 
-#define Max_Size 51
+#define MAX_SIZE 51
 
 using namespace std;
 
-int map[Max_Size][Max_Size];
-int check[Max_Size][Max_Size];
+int map[MAX_SIZE][MAX_SIZE];
+
+
+int turn(int now)
+{
+    return (now + 1) % 4; // 0->1 1->2 2->3 3->0
+}
 
 int main(){
 	
 	int N, M, r, c, d;
 	int dx[] = {0, 1, 0, -1};
 	int dy[] = {1, 0, -1, 0};
-	int x, y;
+	int nx, ny;
+	int cx, cy, cd, turnCount=0;
+	int result=0;
 	cin >> N >> M >>r >> c >> d;
 	
 	//빈칸, 벽 세팅 
@@ -20,58 +27,51 @@ int main(){
 		for(int j = 0 ; j<M; j++){
 			cin >> map[i][j];
 		}
-	}
-	
-	if(d == 0){ //북 
-		x = dx[0];
-		y = dy[0];
-		break;
-	}
-	else if(d==1){ //동 
-		x = dx[1];
-		y = dy[1];
-		break;
-	}
-	else if(d==2){ //남 
-		x = dx[2];
-		y = dy[2];
-		break;
-	}
-	else if(d==3){ //서 
-		x = dx[3];
-		y = dy[3];
-		break;
-	}
-	
-	while(){
-		check[r][c]=1;//현재 위치 청소 
-		
-		for(int i = 0 ; i< 4; i++){
-			if(d>0){
-				d--;
-			}
-			else{
-				d=3;
-			}
-			
-			nx=c+dx[d];
-			ny=r+dy[d];
-			
-			if(map[r][c] == 0 && check[r][c] == 0){
-				check[r][c] = 1;
-				continue;
-			}
-			if(map[r][c] == 1){
-				r-=x;
-				c-=y;
-				continue;
-			}
-			else if(map[
-				
-		} 
-	}
+	}	
+	cx = c;
+	cy = r;
+	cd = d;
 	
 	
-	
+	result ++ ;
+	while(1){
+		if(turnCount == 4)
+        {
+            d = turn(cd);
+            nx = cx + dx[cd];
+            ny = cy + dy[cd];
+ 
+            if(nx < 0 || ny < 0 || nx >= M || ny >= N || map[nx][ny] == 1) break;
+ 
+            cx = nx;
+            cy = ny;
+            cd = turn(cd);
+            turnCount = 0;
+            continue;
+        }
+ 
+        cd = turn(cd);
+        nx = cx + dx[cd];
+        ny = cy + dy[cd];
+ 
+        turnCount++;
+ 
+ 
+        if(nx < 0 || ny < 0 || nx >= M || ny >= N || map[nx][ny] != 0) continue;
+ 
+        cx = nx;
+        cy = ny;
+ 
+        map[cy][cx] = 2; // 청소함!
+ 
+        result++;
+        turnCount = 0;					
+	} 
+	cout << result;
 	return 0;
 }
+	
+
+	
+
+
